ccbb389257f1f43632d6bcd62446ffa17a5d988b
geeksbaek <geeksbaek@gmail.com>
2012-10-20 12:13:22 +0900

    프리뷰 화면에서 드래그로 순서 변경
    다듬을 필요 있음

--- public/javascripts/UnlimitedImageCombiner.js
+++ public/javascripts/UnlimitedImageCombiner.js
@@ -5,17 +5,17 @@ function init() {
   // 폭 버튼을 누르면 이미지 합침
   [].forEach.call($('#radio>button'), function (element) {
     $(element).bind('click', function () {
-      if (window.files != undefined && window.files.length > 1) {
-        handleFiles();
+      if (window.UIC.files != undefined && window.UIC.files.length > 1) {
+        combineImage();
       }
     });
   });
 
   // 수동 폭 입력 폼에서 엔터를 누르면 이미지 합침
   $('#width').keyup(function (e) {
-    if (window.files != undefined && window.files.length > 1) {
+    if (window.UIC.files != undefined && window.UIC.files.length > 1) {
       if (e.keyCode == 13 && $('#width').val() != '') {
-        handleFiles();
+        combineImage();
       }
     }
   });
@@ -46,121 +46,191 @@ function init() {
   $('.close').click(function () { $(this).parent().fadeOut(200); });
   $('#introduce_message').center().fadeIn(300);
 
+  $('.navbar-inner').css('-webkit-transition', 'height .5s ease 0s');
+  $('.navbar-inner').css('-moz-transition', 'height .5s ease 0s');
+  $('.navbar-inner').css('transition', 'height .5s ease 0s');
+
   $('#clear').bind('click', allClear);
-  $('#combine').bind('click', handleFiles);
+  $('#combine').bind('click', combineImage);
   $('#download').bind('click', download);
 
   allClear();
 }
 
 function uploadEvent(input) {
-  [].forEach.call(input, function (element) { window.files.push(element); });
+  if (input.length == 0) return;
+
+  for (var i = 0, max = input.length; i < max; i++) {
+    $('#preview').append('<img>');
+  }
+
+  [].forEach.call(input, function (element) {
+    UIC.files.push(element);
+    addPreview(element);
+  });
+
+  UIC.loader.start();
+
   $('#clear').removeClass('disabled');
   $('#combine').removeClass('disabled');
+  $('.navbar-inner').css('height', '175px');
+}
+
+function addPreview(_files) {
+  var imgaeURL = URL.createObjectURL(_files);
+  var pxImage = new PxLoaderImage(imgaeURL);
+  pxImage.imageNumber = UIC.count++;
+  UIC.loader.add(pxImage);
 }
 
 function allClear() {
-  $('canvas').remove();
+  $('#content>canvas').remove();
+  $('#preview>img').remove();
   $('#buttons>button').addClass('disabled');
-  window.files = [];
+  $('.navbar-inner').css('height', '50px');
+
+  window.UIC = {
+    files: [],
+    images: [],
+    maxWidth: Number.MIN_VALUE,
+    minWidth: Number.MAX_VALUE,
+    loader: new PxLoader,
+    count: 0,
+    dragSrcEl: null
+  };
+
+  UIC.loader.addProgressListener(function (e) {
+    UIC.maxWidth = Math.max(UIC.maxWidth, e.resource.img.width);
+    UIC.minWidth = Math.min(UIC.minWidth, e.resource.img.width);
+    UIC.images[e.resource.imageNumber] = { src: e.resource.img, height: e.resource.img.height };
+
+    var img = $('#preview>img')[e.resource.imageNumber];
+
+    $(img).attr('src', e.resource.img.src);
+    img.addEventListener('dragstart', handleDragStart, false);
+    img.addEventListener('dragenter', handleDragEnter, false);
+    img.addEventListener('dragover', handleDragOver, false);
+    img.addEventListener('dragleave', handleDragLeave, false);
+    img.addEventListener('drop', handleDrop, false);
+    img.addEventListener('dragend', handleDragEnd, false);
+  });
 }
 
-function resizeEvent() {
-  [].forEach.call($('.alert'), function (element) { $(element).center(); });
+function handleDragStart(e) {
+  $(this).css('opacity', '0.4');
+
+  UIC.dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', $(this).attr('src'));
 }
 
-function handleFiles() {
-  if (window.files.length == 0) return;
+function handleDragEnter(e) {
+  $(this).addClass('over');
+}
 
-  $('.alert').hide();
-  $('canvas').remove();
+function handleDragOver(e) {
+  if (e.preventDefault) { e.preventDefault(); }
+  e.dataTransfer.dropEffect = 'move';
 
-  var images = [];
-  var maxWidth = Number.MIN_VALUE, minWidth = Number.MAX_VALUE;
-  var sumHeight = [{ height: 0, count: 0 }], sumHeightIndex = 0, realSumHeight = 0;
-  var loader = new PxLoader;
+  return false;
+}
 
-  loader.addProgressListener(function (e) {
-    maxWidth = Math.max(maxWidth, e.resource.img.width);
-    minWidth = Math.min(minWidth, e.resource.img.width);
-    images[e.resource.imageNumber] = { src: e.resource.img, height: e.resource.img.height };
-  });
+function handleDragLeave(e) {
+  $(this).removeClass('over');
+}
 
-  loader.addCompletionListener(function (e) {
-    var heights = [];
-    var devideValue = $('#radio>.active').text() == '수동 폭' ?
-      $('#width').val() : $('#radio>.active').text() == '자동 최대 폭' ?
-      maxWidth : minWidth;
-    var gap = 2;
+function handleDrop(e) {
+  if (e.stopPropagation) { e.stopPropagation(); }
 
-    // 이미지들의 최종 높이 = realSumHeight
-    // 캔버스의 개수 = sumHeightIndex
-    // 각 캔버스의 높이와 캔버스에 들어갈 이미지의 개수 = sumHeight
-    for (var i = 0, max = images.length; i < max; i++) {
-      heights.push(images[i].src.height / (images[i].src.width / devideValue));
-
-      if (sumHeight[sumHeightIndex].height + heights[i] + gap > 32000) { // 여기
-        sumHeightIndex++;
-        sumHeight[sumHeightIndex] = { height: 0, count: 0 };
-      }
-
-      sumHeight[sumHeightIndex].height += heights[i] + gap; // 여기
-      sumHeight[sumHeightIndex].count++;
-      realSumHeight += heights[i];
-    }
-
-    for (var i = 0; i <= sumHeightIndex; i++) {
-      $('#content').append('<canvas />');
-    }
-
-    var canvas = document.querySelectorAll('canvas');
-    var ctx = [];
-
-    [].forEach.call(canvas, function (element, index) {
-      ctx[index] = element.getContext('2d');
-    });
-
-    for (var i = 0, totalCount = 0; i < canvas.length; i++) {
-      canvas[i].width = devideValue;
-      canvas[i].height = sumHeight[i].height - gap; // 여기
-
-      ctx[i].save();
-      ctx[i].fillStyle = 'white';
-      ctx[i].fillRect(0, 0, canvas[i].width, canvas[i].height);
-      ctx[i].restore();
-
-      for (var j = 0, max = sumHeight[i].count, accrueHeight = 0; j < max; j++) {
-        ctx[i].drawImage(images[totalCount].src, 0, accrueHeight, devideValue, heights[totalCount]);
-        accrueHeight += heights[totalCount] + (j == max - 1 ? 0 : gap); // 여기
-        totalCount++;
-      }
-    }
-
-    if (sumHeightIndex > 0) {
-      $('#success_multiple>p').first().text("결과물의 높이가 " + parseInt(realSumHeight) +
-        "px로, 저희가 지원하는 단일 파일 최대 높이인 32000px을 초과했기 때문에 " +
-        (sumHeightIndex + 1) + "개의 파일로 분할되어 합쳐졌습니다.");
-      $('#success_multiple').center().show();
-    } else {
-      $('#success').center().show();
-    }
-
-    $('#download').removeClass('disabled');
-    $('canvas').show();
-  });
-
-  for (var i = 0, max = window.files.length; i < max; i++) {
-    var imgaeURL = URL.createObjectURL(window.files[i]);
-    var pxImage = new PxLoaderImage(imgaeURL);
-    pxImage.imageNumber = i;
-    loader.add(pxImage);
+  if (UIC.dragSrcEl != this) {
+    $(UIC.dragSrcEl).attr('src', $(this).attr('src'));
+    $(this).attr('src', e.dataTransfer.getData('text/html'));
   }
 
-  loader.start();
+  return false;
+}
+
+function handleDragEnd(e) {
+  [].forEach.call($('#preview>img'), function (element) {
+    $(element).css('opacity', '1');
+    $(element).removeClass('over');
+  });
+}
+
+function combineImage() {
+  if (UIC.files.length == 0) return;
+
+  $('.alert').hide();
+  $('#content>canvas').remove();
+
+  var heights = [];
+  var devideValue = $('#radio>.active').text() == '수동 폭' ?
+    $('#width').val() : $('#radio>.active').text() == '자동 최대 폭' ?
+    UIC.maxWidth : UIC.minWidth;
+  var gap = 2;
+  var sumHeight = [{ height: 0, count: 0 }];
+  var sumHeightIndex = 0;
+  var realSumHeight = 0;
+
+  // 이미지들의 최종 높이 = realSumHeight
+  // 캔버스의 개수 = sumHeightIndex
+  // 각 캔버스의 높이와 캔버스에 들어갈 이미지의 개수 = sumHeight
+  for (var i = 0, max = UIC.images.length; i < max; i++) {
+    heights.push(UIC.images[i].src.height / (UIC.images[i].src.width / devideValue));
+
+    if (sumHeight[sumHeightIndex].height + heights[i] + gap > 32000) { // 여기
+      sumHeightIndex++;
+      sumHeight[sumHeightIndex] = { height: 0, count: 0 };
+    }
+
+    sumHeight[sumHeightIndex].height += heights[i] + gap; // 여기
+    sumHeight[sumHeightIndex].count++;
+    realSumHeight += heights[i];
+  }
+
+  for (var i = 0; i <= sumHeightIndex; i++) {
+    $('#content').append('<canvas />');
+  }
+
+  var canvas = document.querySelectorAll('#content>canvas');
+  var ctx = [];
+
+  [].forEach.call(canvas, function (element, index) {
+    ctx[index] = element.getContext('2d');
+  });
+
+  for (var i = 0, totalCount = 0; i < canvas.length; i++) {
+    canvas[i].width = devideValue;
+    canvas[i].height = sumHeight[i].height - gap; // 여기
+
+    ctx[i].save();
+    ctx[i].fillStyle = 'white'; // 배경색
+    ctx[i].fillRect(0, 0, canvas[i].width, canvas[i].height);
+    ctx[i].restore();
+
+    for (var j = 0, max = sumHeight[i].count, accrueHeight = 0; j < max; j++) {
+      ctx[i].drawImage(UIC.images[totalCount].src, 0, accrueHeight, devideValue, heights[totalCount]);
+      accrueHeight += heights[totalCount] + (j == max - 1 ? 0 : gap); // 여기
+      totalCount++;
+    }
+  }
+
+  if (sumHeightIndex > 0) {
+    $('#success_multiple>p').first().text("결과물의 높이가 " + parseInt(realSumHeight) +
+      "px로, 저희가 지원하는 단일 파일 최대 높이인 32000px을 초과했기 때문에 " +
+      (sumHeightIndex + 1) + "개의 파일로 분할되어 합쳐졌습니다.");
+    $('#success_multiple').center().show();
+  } else {
+    $('#success').center().show();
+  }
+
+  $('#download').removeClass('disabled');
+  $('#content>canvas').show();
 }
 
 function download() {
-  [].forEach.call(document.querySelectorAll('canvas'), function (element, index) {
+  [].forEach.call(document.querySelectorAll('#content>canvas'), function (element, index) {
     element.toBlob(function (blob) {
       if (window.saveAs) {
         window.saveAs(blob, "result " + (index + 1) + ".png");
@@ -169,6 +239,10 @@ function download() {
       }
     });
   });
+}
+
+function resizeEvent() {
+  [].forEach.call($('.alert'), function (element) { $(element).center(); });
 }
 
 jQuery.fn.center = function () {
